6cdd9a30083d7d7046876f4d49ab12c70b8780c6
Jonathan Eom <jonnyeom@gmail.com>
2019-12-28 21:46:54 -0500

    Initial customizations

--- config.js
+++ config.js
@@ -1,36 +1,15 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Multiverse', // <title>
-  manifestName: 'Multiverse',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'jonnyeom | photos', // <title>
+  manifestName: 'jonnyeom | photos',
+  manifestShortName: 'photos', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-multiverse/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav',
-  subHeading: 'Web Developer',
+  heading: 'jonnyeom',
+  subHeading: 'what ig could have been',
   // social
-  socialLinks: [
-    {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
-    },
-    {
-      icon: 'fa-envelope-o',
-      name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
-    },
-  ],
+  socialLinks: [],
 };
